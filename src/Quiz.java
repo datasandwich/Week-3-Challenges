8747ea51988c98c12142aa9498726242703cdb9d
datasandwich <72459044+datasandwich@users.noreply.github.com>
2022-05-09 15:37:19 +0100

    Initial commit

---
+++ src/Quiz.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Quiz {
+    public static void main(String[] args) {
+        /*
+
+         */
+        String q1 = "What is the highest mountain in the world?";
+        String q2 = "What is the longest river in the world?";
+        String q3 = "What is the fastest the land-animal?";
+
+        String a1_c = "Everest";
+        String a1_w_1 = "Kilimanjaro";
+        String a1_w_2 = "Makalu";
+        String a1_w_3 = "Kangchenjunga";
+
+        String a2_c = "Nile";
+        String a2_w_1 = "Danube";
+        String a2_w_2 = "Thames";
+        String a2_w_3 = "Amazon";
+
+        String a3_c = "Cheetah";
+        String a3_w_1 = "Lion";
+        String a3_w_2 = "Leopard";
+        String a3_w_3 = "Ostrich";
+
+                //,"Kilimanjaro","Kanchengjunga","Makalu"};
+        String[][] questionsAndAnswers =
+                {{q1,a1_c,a1_w_1,a1_w_2,a1_w_3},
+                        {q2,a2_c,a2_w_1,a2_w_2,a2_w_3},
+                        {q3,a3_c,a3_w_1,a3_w_2,a3_w_3}};
+        /*
+        for (String[] questionsAndAnswer : questionsAndAnswers) {
+            for (String s : questionsAndAnswer) {
+                System.out.println(s);
+            }
+        }
+        String x = questionsAndAnswers[0][1];
+        System.out.println(x);
+        */
+
+
+
+        for (String[] questionsAndAnswer : questionsAndAnswers) {
+            boolean correct = false;
+            while (!correct) {
+                for (String s : questionsAndAnswer) {
+                    System.out.println(s);
+                }
+                Scanner scan = new Scanner(System.in);
+                String answer = scan.nextLine();
+                if (answer.equals(questionsAndAnswer[1])){
+                    System.out.println("correct");
+                    correct= true;
+                } else {
+                    System.out.println("Try again!");
+                }
+            }
+        }
+
+    }
+}
